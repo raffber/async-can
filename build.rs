99c78743dffe8a28250defd484a402d674f0d666
Raphael Bernhard <beraphae@gmail.com>
2023-01-02 00:58:46 +0100

    build: Only run bindgen if PCAN feature is selected

--- build.rs
+++ build.rs
@@ -1,17 +1,19 @@
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
-    use std::env;
-    use std::path::PathBuf;
+    if env::var("CARGO_FEATURE_PCAN").is_ok() {
+        println!("cargo:rerun-if-changed=include/wrapper.h");
 
-    println!("cargo:rerun-if-changed=include/wrapper.h");
+        let bindings = bindgen::Builder::default()
+            .header("include/wrapper.h")
+            .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+            .generate()
+            .expect("Unable to generate bindings");
 
-    let bindings = bindgen::Builder::default()
-        .header("include/wrapper.h")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .generate()
-        .expect("Unable to generate bindings");
-
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+        let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+        bindings
+            .write_to_file(out_path.join("bindings.rs"))
+            .expect("Couldn't write bindings!");
+    }
 }
